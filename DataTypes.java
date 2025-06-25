fadd0361227bec1cf617f237cd0a57c2f60838a1
Pradee72 <159802057+Pradee72@users.noreply.github.com>
2025-06-25 21:25:13 +0530

    day2
    Day2

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+	public static void main(String[] args) {
+		//integer
+		byte age = 21;
+		System.out.println("byte type is : "+age);
+		short a = 122;
+		System.out.println("short type is : "+a);
+		int EMISNo = 23456477;
+		System.out.println("int type is : "+EMISNo);
+		float bill = 1634.754f;
+		System.out.println("float type is : "+bill);
+		long Phonenumber = 9940396814l;
+		System.out.println("long type is : "+Phonenumber);
+		double b = 5673.543689;
+		System.out.println("double type is : "+b);
+		boolean istrue = true;
+		System.out.println("boolean type is : "+istrue);
+		char b = a;
+		System.out.println("char type is : "+a);
+		
+		
+	}
